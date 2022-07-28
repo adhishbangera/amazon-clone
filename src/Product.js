a7f388f95266ec463b5bf5bbc0223be640ca8dc0
adhishbangera <adhishbangera18@gmail.com>
2022-07-28 11:30:03 +0530

    Initital commit

---
+++ src/Product.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Product.css";
+
+function Product(props) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{props.title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{props.price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(props.rating)
+            .fill()
+            .map((_) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+      <img src={props.image} alt="" />
+      <button>Add to basket</button>
+    </div>
+  );
+}
+
+export default Product;
